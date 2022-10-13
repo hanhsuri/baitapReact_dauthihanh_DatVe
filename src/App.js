468ccb567ef33e3e1b5e98cc4a92f6aa114eb15b
Hanh Suri <lucy051822@gmail.com>
2022-10-13 20:42:58 +0700

    create index.html

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import BaiTapBookingTicket from './BaiTap/BaiTapBookingTicket/BaiTapBookingTicket'
+
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BaiTapBookingTicket />
+      </div>
+
+    )
+  }
+}
